09ca135635504e6d35a254924d69766d0096c98c
Michelle Sri <michelle.sri@gmail.com>
2016-10-20 13:39:41 -0700

    emoji cli working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-//additional entry point for my app that lets you enter in 'node index blah' to get the greeting with blah. 
+//additional entry point for my app that lets you enter in 'node index blah' to get the greeting with blah.
 // if no process.argv[2] specified, default parameter for name is used.
 const greet = require('./lib/greet');
 
-let response = greet(process.argv[2]);
+let response = greet(process.argv[2], process.argv[3]);
 
 console.log(response);
